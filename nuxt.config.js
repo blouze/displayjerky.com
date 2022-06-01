a996713dbd3be808f8d0db870ce72792128f3699
Blaise <blouze@gmail.com>
2022-06-01 15:18:28 +0200

    router base config not needed with custom domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,9 +61,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  },
-
-  router: {
-    base: process.env.NODE_ENV !== 'production' ? '' : '/displayjerky.com/'
   }
 }
